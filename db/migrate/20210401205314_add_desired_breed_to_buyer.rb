88cd24a7448aafd4c2198b85abdc61c16bd733fc
jamesElls <jamellsworth@gmail.com>
2021-04-01 17:12:42 -0600

    accordian drop down

---
+++ db/migrate/20210401205314_add_desired_breed_to_buyer.rb
@@ -0,0 +1,4 @@
+class AddDesiredBreedToBuyer < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
---
+++ db/migrate/20210401205344_add_desired_breed_to_buyers.rb
@@ -0,0 +1,4 @@
+class AddDesiredBreedToBuyers < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
