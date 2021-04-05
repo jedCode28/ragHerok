8791de510dedc32c1c127b8e36c2a4699c118ae2
Jedediah Wood <jedediahwood28@gmail.com>
2021-04-05 16:37:34 -0600

    Home unfinished

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -18,7 +18,13 @@ const Home = () => {
   }
   return (
     <div>
-    <header as='h1' textAlign='center'>Login in/ register</header>
+    <h1>~Welcome To The Etsy Dog Trade~</h1>
+    <h2>Shop Now!</h2>
+    <div style={{display: 'flex'}}>
+    <img src="https://static.simpsonswiki.com/images/thumb/4/4b/Wacky_Dog.png/393px-Wacky_Dog.png" />
+    {/* <img src="https://i.stack.imgur.com/YK230.png" style={{height: '250px'}} /> */}
+    <h2>P-Please Don't Leave Me Here</h2>
+    </div>
     </div>
   )
 }
